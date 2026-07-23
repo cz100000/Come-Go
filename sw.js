afc4e4898243a963b2aaa0a83b14dfb2878227df
cz100000 <cz151283@gmail.com>
2026-07-23 19:12:09 +0200

    Arbeitszeit_PWA_Offline_V5_4_Korrektur6_HighEnd_iPhone16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='arbeitszeit-v5-4-korrektur5-startseite-2026-07-23';
+const CACHE='arbeitszeit-v5-4-korrektur6-startseite-2026-07-23';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png','./icon-1024.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
